94b9ae7f3ca8c4c7d1f7b636a213254efda5ab2b
Alisson Zampietro <alisson@mentesnotaveis.com.br>
2017-02-15 21:07:56 -0200

    Finalizando extension transnow

--- popup.js
+++ popup.js
@@ -1,14 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
-function click(e) {
-  chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-  window.close();
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.executeScript( {
       code: "window.getSelection().toString();"
