fb2c838bc18db4747e3a69160616fe79e47b9d08
Alisson Zampietro <alisson@mentesnotaveis.com.br>
2017-02-15 17:51:26 -0200

    Implementando request ajax

--- popup.js
+++ popup.js
@@ -10,8 +10,18 @@ function click(e) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
-  }
+  chrome.tabs.executeScript( {
+      code: "window.getSelection().toString();"
+  }, function(selection) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var jsonReturned = JSON.parse(this.responseText);
+          document.getElementById("res").innerHTML = jsonReturned.translator;
+        }
+      };
+      xhttp.open("GET", "www.alzaro.com.br:8080/"+selection[0], true);
+      xhttp.send();
+  });
+
 });
