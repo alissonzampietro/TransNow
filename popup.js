72023987d64fa4a43609f47ff091012a678e089b
Alisson Zampietro <alisson@mentesnotaveis.com.br>
2017-02-16 16:28:01 -0200

    Desenvolvendo mensagem de bem vindo

--- popup.js
+++ popup.js
@@ -2,35 +2,25 @@ document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.executeScript( {
       code: "window.getSelection().toString();"
   }, function(selection) {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          var jsonReturned = JSON.parse(this.responseText);
-          document.getElementById("res").innerHTML = jsonReturned.translator;
-        }
-      };
-      xhttp.open("GET", "http://apiserver/"+selection[0], true);
-      xhttp.send();
+      if(selection[0].length == 0){
+        welcomeMessage();
+      }else {
+        document.getElementById("res").innerHTML = "traduzindo...";
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var jsonReturned = JSON.parse(this.responseText);
+            document.getElementById("res").innerHTML = jsonReturned.translator;
+          }
+        };
+        xhttp.open("GET", "http://apitransnow/"+selection[0], true);
+        xhttp.send();
+      }
   });
 
 });
 
-var interrogacao = document.getElementById("messages");
-
-var changeInfos = function(remove, add, message){
-    interrogacao.classList.remove(remove);
-    interrogacao.classList.add(add);
-    interrogacao.innerHTML = message;
-}
-
-var toogle = function()
+var welcomeMessage = function()
 {
-  if(interrogacao.classList.contains("help"))
-  {
-    changeInfos('help','info','Ajuda: Selecione o texto em Inglês e depois clique no meu ícone ;-)');
-  }else{
-    changeInfos('info','help','?');
-  }
+  document.getElementById("msgMain").innerHTML = "Bem vindo!";
 }
-
-interrogacao.addEventListener("click", toogle);
