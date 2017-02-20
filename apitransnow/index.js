de71d4f8680e6c8d61ebc04b9d842c518ff1df0a
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-02-20 13:00:19 -0300

    Starting multilanguage feature

--- apitransnow/index.js
+++ apitransnow/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 const translate = require('google-translate-api');
+var languageDetect = require('langdetect');
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,7 +14,8 @@ app.use(function (req, res, next) {
 
 app.get('/:q', function(req,res){
 	var q = req.params.q;
-		translate(q, {from: 'en',to: 'pt'}).then(response => {
+	var obj = languageDetect.detect(q);
+		translate(q, {from: obj[0].lang,to: 'pt'}).then(response => {
 			res.json({"translator":response.text});
 		}).catch(err => {
 		    console.error(err);
