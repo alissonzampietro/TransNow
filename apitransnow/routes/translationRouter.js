5919c05d5561829b3f0a60033916900683c5d274
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-05-11 15:21:51 +0100

    feaat: add welcome message

--- apitransnow/routes/translationRouter.js
+++ apitransnow/routes/translationRouter.js
@@ -2,6 +2,10 @@ const express = require("express");
 const translationController = require("../controller/translateController.js");
 const router = express.Router();
 
-router.get("/:q", translationController.getTranslation);
+router.get("/", (req,res) => {
+	res.json({'welcome to': 'transnow api'});
+});
+
+router.post("/:q", translationController.getTranslation);
 
 module.exports = router;
